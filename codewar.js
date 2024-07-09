68a444565f5a0f0a426f9d6df5d8078abf746425
angep72 <p.umunyana@alustudent.com>
2024-07-09 16:36:16 +0200

    second uestiond

--- codewar.js
+++ codewar.js
@@ -47,4 +47,32 @@ exports.users.forEach(logPerson);
 // In case you are stuck:
 // https://www.typescriptlang.org/docs/handbook/2/objects.html
 
-    
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.persons = void 0;
+exports.logPerson = logPerson;
+exports.persons /* <- Person[] */ = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+function logPerson(user) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+exports.persons.forEach(logPerson);
