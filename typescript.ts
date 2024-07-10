78f235f298397fa873196e877b052e8b218bf4f2
angep72 <p.umunyana@alustudent.com>
2024-07-10 15:30:06 +0200

    second example using aliases in typescript

--- typescript.ts
+++ typescript.ts
@@ -13,3 +13,32 @@ function myCount (arr:number[]){
     return count
 }
 console.log(myCount([12,3,4,5,6,7,5,4,5,4,5,4,5]))
+// Second challenge on typescript 
+type Employee ={
+    id:number;
+    name:string;
+   department:string;
+}
+type Manager={
+    id:number;
+    name:string;
+    employees:Employee[];
+}
+type Staff = Employee| Manager;
+function printstaff(student:Staff){
+    if ('employees' in student){
+        console.log(`this person is manager ${student.employees.length}`)
+    }
+}
+const kii:Employee={
+    name:"Ange",
+    id:34,
+    department:"eng"
+}
+const Aline : Manager={
+    id:23,
+    name:"Pauline",
+    employees:[kii]
+}
+
+console.log(printstaff(Aline))
